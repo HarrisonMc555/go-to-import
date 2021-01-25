2bb03ff2d322850758d659eea9d16eb64eb4bf0c
GitHub Action <action@github.com>
2021-01-25 21:37:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/harrisonmc555/gotoimport/MyBundle.kt
+++ src/main/kotlin/com/github/harrisonmc555/gotoimport/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.harrisonmc555.gotoimport
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/harrisonmc555/gotoimport/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/harrisonmc555/gotoimport/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.harrisonmc555.gotoimport.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.harrisonmc555.gotoimport.MyBundle
 
 class MyApplicationService {
 
